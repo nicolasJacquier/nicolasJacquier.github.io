e8b0b3cdc7808730d7218f5dfb4240b67aa2162d
nicolasJacquier <nicolas.jacquier@edu.esiee.fr>
2017-06-26 01:35:29 +0300

    functionning print on a browser of data, starting to use ajax and jquery now

--- client_.js
+++ client_.js
@@ -1,15 +1,19 @@
-var Client = require('node-rest-client').Client;
-var client = new Client();
-// direct way 
-var request = require('request');
+function send_data()
+{
+	var Client = require('node-rest-client').Client;
+	var client = new Client();
+	// direct way 
+	var request = require('request');
 
-request.post(
-    'http://127.0.0.1:8081/List',
-    { json: { 'filename': "17.json","count":"6"} },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            for(var i=0;i<body.length; i++)
-			 console.log(body[i]._id);
-        }
-    }
-);
+	request.post(
+		'http://127.0.0.1:8081/',
+		{ json: { 'filename': "18.json","count":"1"} },
+		function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				for(var i=0;i<body.length; i++)
+				 console.log(body[i]._id);
+			}
+		}
+	);
+
+}
--- test_.js
+++ test_.js
@@ -4,6 +4,7 @@ var finalData=[];
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser')
+var cors = require('cors');
 var tmp_ = []
 JSONStream = require('JSONStream'),
 es = require('event-stream');
@@ -18,30 +19,36 @@ var server = app.listen(8081, function ()
 })
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/client_paga_.html'));
 });
 
-app.post('/List', function (req, res) {
+app.post('/', function (req, res) {
 	var bodyStr = '';	
+	finalData=[];
 	counter_ = 0
 	req.on("data",function(chunk){ bodyStr += chunk.toString(); });
     req.on("end",function(){
-		req_parsed_ = JSON.parse(bodyStr)
+		
+		req_parsed_ = JSON.parse(bodyStr);
 		//fname_ = getFname(req_parsed_.filename);
 		var stream = fs.createReadStream(req_parsed_.filename);
+		
+		
 		var getStream = function () {
 			parser = JSONStream.parse(['posts', true]);
 			return stream.pipe(parser);
 		};
+		
 		getStream().pipe(es.mapSync(function(data){
 		 if(counter_ < req_parsed_.count)
 		   finalData.push(data);
 		 else
 		 {
+			 
 		   stream.unpipe();
 		   res.end( JSON.stringify(finalData));
 		 }
 		 counter_+=1
 		}));
-	});		
+	}); 
 });
