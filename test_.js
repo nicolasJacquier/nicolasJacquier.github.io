27d2968ce76875ba99d5a2fdb85c8484c633e8d9
nicolasJacquier <nicolas.jacquier@edu.esiee.fr>
2017-07-19 17:39:21 +0300

    Trying to get last chunk, starting to look at MapBox

--- test_.js
+++ test_.js
@@ -17,96 +17,116 @@ var fname_ = '';
 var isFirstPost = true;
 var is_first_post_ = true;
 var post_counter = 0;
+var global_counter =0;
+var rec__counter = 0;
+var size_ = 0;
+var last_chunk_size = 0;
 
 var server = app.listen(8081, function () 
 {
   var host = server.address().address
   var port = server.address().port
 })
-
 app.get('/', function(req, res) 
-{
-    res.sendFile(path.join(__dirname + '/index_main.html'));
+{ 
+	res.sendFile(path.join(__dirname + '/index_main.html')); 
 });
 
 app.post('/', function (req, res) 
+{
+	var bodyStr = '';
+	req.on("data",function(chunk){ bodyStr += chunk.toString();});
+	req.on("end",function(){
+		req_parsed_ = JSON.parse(bodyStr)
+		if(post_counter == 0) { readFile(req_parsed_.filename, req_parsed_.count, req, res); }
+		else { post__main_fun(req_parsed_.count, global_counter, req_parsed_.filename, req, res); }
+	});
+	
+});
+
+function post__main_fun(chunk_sizeClient, total__count, fname, req__, res__)
 {
 	var bodyStr = '';	
 	counter_ = 0;
 	tmp_prevCount = 0;
 	finalData=[];
 	post_counter += 1;
-	req.on("data",function(chunk){ bodyStr += chunk.toString();});
-	req.on("end",function(){
-		req_parsed_ = JSON.parse(bodyStr)
-		__counter__ += parseInt(req_parsed_.count);
-
-		console.log(__counter__);
-
-		var stream = fs.createReadStream(req_parsed_.filename);
-		var getStream = function () 
+		var stream__ = fs.createReadStream(fname);
+		var getStream__ = function () 
 		{
-			stream.on('end',()=> {
-				parser = JSONStream.parse(['posts', true]);
-			return stream.pipe(parser);
-				stream.unpipe();
-				res.end( JSON.stringify(["EOF"]));
-			});
-			
 			parser = JSONStream.parse(['posts', true]);
-			return stream.pipe(parser);
+			return stream__.pipe(parser);
 		};
-			getStream().pipe(es.mapSync(function(data)
-			{		
-			        //if(data.geo != undefined)
-						//console.log(data.geo.coordinates);
+		
+		/*****************************************************************************************************/
+		/// STREAMING THE FILE ////
+		
+		getStream__().pipe(es.mapSync(function(data)
+		{		
+				if(is_first_post_ == true)
+				{
+					if((counter_ < chunk_sizeClient) && (is_first_post_ == true))
+					{ finalData.push(data);  }
+					if((counter_ == chunk_sizeClient) && (is_first_post_ == true))
+					{ 
+						 //finalData.push(["EOF"]);
+						 res__.end( JSON.stringify(finalData));
+						 finalData = [];
+						 prevCount = counter_;
+						 stream__.unpipe();
+						 is_first_post_ = false;
+					} 
+					counter_+=1		
+				}		  
+				
+				
+				if(post_counter > 1 && is_first_post_ == false)
+				{
 					
-					if(is_first_post_ == true)
-					{
-						if((counter_ < __counter__) && (is_first_post_ == true))
-							finalData.push(data);
-						if((counter_ == __counter__) && (is_first_post_ == true))
-						{ 
-							 prevCount = counter_
-							 stream.unpipe();
-							 res.end( JSON.stringify(finalData));
-							 finalData=[];
-							 is_first_post_ = false;
-						} 
-						counter_+=1		
-					}		  
+					if(chunk_sizeClient > (global_counter - prevCount)){ 
 					
+						console.log("NEAR TO END ... ", counter_, (global_counter - prevCount), (parseInt(prevCount) + parseInt(chunk_sizeClient)));
+						last_chunk_size = (global_counter - prevCount) - 2;
+						console.log("Last chunk "+ last_chunk_size);
+						finalData.push(["EOF"]);
 					
-					if(post_counter > 1 && is_first_post_ == false)
-					{
-						//stream.unpipe();
-						
-						console.log(req_parsed_.count, prevCount);
-						if(counter_ == __counter__)
-						{	
-							stream.unpipe(); 
-							prevCount = counter_;
-							stream.unpipe();
-							res.end( JSON.stringify(finalData));
-							console.log(finalData);
-							finalData=[];
-						}
-						
-						if(counter_ > prevCount)
-						{
-							//console.log(counter_);
-							finalData.push(data);
-						}
-						console.log(k);
-						counter_ += 1
-						k+=1
 					}
-			})); 		
-	});
-});
-
-function ff(dat)
-{
-	console.log(dat);
-	
+					
+					if(counter_ == (parseInt(prevCount) + parseInt(chunk_sizeClient)) && chunk_sizeClient < (global_counter - prevCount))
+					{	
+						stream__.unpipe(); 
+						prevCount = counter_;
+						stream__.unpipe();
+						res__.end( JSON.stringify(finalData));
+					}
+					
+					if(counter_ >= prevCount) finalData.push(data); 
+					counter_ += 1
+				}
+		})); 	
 }
+
+function readFile(param, client_num_rec_req, request__, resp__)
+{	
+	var stream = fs.createReadStream(req_parsed_.filename);
+	var getStream = function () 
+	{
+		stream.on('end',()=> {
+			parser = JSONStream.parse(['posts', true]);
+		return stream.pipe(parser);
+			stream.unpipe();
+			res.end( JSON.stringify(["EOF"]));
+		});
+		
+		parser = JSONStream.parse(['posts', true]);
+		return stream.pipe(parser);
+	};
+	getStream().pipe(es.mapSync(function(data)
+	{global_counter+=1;}));
+	stream.on('end',()=> {
+			stream.unpipe();
+			global_counter -= 1;
+			post__main_fun(client_num_rec_req, global_counter, param, request__, resp__);
+	});
+	
+};
